c90937da29e75ed901202ab6d3f2a4697783e00a
jmrvicencio <35318735+jmrvicencio@users.noreply.github.com>
2025-09-03 23:31:11 +0800

    💄 Added loading icon on ProtectedRoute
    logo loading icon shows when the app is checking the auth.uid

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -3,6 +3,8 @@ import { auth } from '../firebase/auth';
 import { onAuthStateChanged, type User } from 'firebase/auth';
 import { useState, useEffect, type ReactNode } from 'react';
 
+import logo from '/logo-spaced-white.svg';
+
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
@@ -18,7 +20,12 @@ function ProtectedRoute({ children }: { children: React.ReactNode }) {
     awaitAuth();
   });
 
-  if (loading) return <div className="bg-olive text-cream h-dvh">Loading...</div>;
+  if (loading)
+    return (
+      <div className="bg-olive text-cream flex h-dvh w-dvw items-center justify-center">
+        <img src={logo} className="w-16 animate-pulse" />
+      </div>
+    );
   else
     return (import.meta.env.VITE_USE_EMULATORS === 'true' && user) ||
       (user && user.uid === 'NgjgtqXPihQSLQfhb2Slc8POVkm1') ? (
