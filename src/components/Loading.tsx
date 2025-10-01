084a8741e8b61d564c244a9e013c4777ff9b5ae8
jmrvicencio <35318735+jmrvicencio@users.noreply.github.com>
2025-10-01 21:40:23 +0800

    💄 Fixed loading screen not being centered

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import Logo from './logo/Logo';
 
 function Loading() {
   return (
-    <div className="text-ink-800 bg flex h-dvh w-dvw items-center justify-center">
+    <div className="text-ink-800 bg flex h-dvh w-full items-center justify-center">
       <Logo className="fill-wheat-400 w-16 animate-pulse" />
     </div>
   );
