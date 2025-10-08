5b016c37d174d77a460c7974ffedd0897314954e
jmrvicencio <35318735+jmrvicencio@users.noreply.github.com>
2025-10-09 03:02:50 +0800

    🧪 Added NewTransaction test to check for group name

--- testing/newTransaction.test.tsx
+++ testing/newTransaction.test.tsx
@@ -1,15 +1,37 @@
-import { describe, it, test, expect } from 'vitest';
-import { render } from '@testing-library/react';
-import NewTransaction from '../src/app/routes/App/NewTransaction';
+import { describe, it, test, expect, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { Group } from '../src/features/groups/types';
+
+// Mock the module before it gets imported
+vi.mock('../src/features/groups/hooks/useGroups', () => ({
+  useGroups: vi.fn(() => ({
+    groups: [
+      {
+        id: 'abcd',
+        data: (): Group => ({
+          balance: {},
+          name: 'Test Group',
+          memberUids: [],
+          members: {},
+        }),
+      },
+    ],
+    loading: false,
+    reload: vi.fn(),
+  })),
+}));
+
+import NewTransaction from '../src/app/routes/App/NewTransaction';
 
 describe('New Transaction Page', () => {
-  it('allows users to input transaction amount', async () => {
+  it('Groups section displays the name of the current active group', async () => {
     const { container } = render(
       <MemoryRouter>
         <NewTransaction />
       </MemoryRouter>,
     );
-    expect(container).toMatchSnapshot();
+
+    expect(screen.getByRole('button', { name: 'Test Group' })).toBeInTheDocument();
   });
 });
